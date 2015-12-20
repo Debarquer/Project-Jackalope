3efc1a2adfcb1551b13e476849eb0a5bd8bd7ab1
Jakobzzz <jakob.ahrneteg@gmail.com>
2015-12-20 17:11:30 +0100

    Removed existing source and header files - replaced with basic windows main

--- Project Jackalope/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-
-#include "Project Jackalope\WarmMilk.h"
-
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	WarmMilk hotMilk;
-	return 0;
-}
