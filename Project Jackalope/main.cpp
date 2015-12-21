6a33f5252f73ae8cd4bf8f1301eeb784551d3e1f
Jakobzzz <jakob.ahrneteg@gmail.com>
2015-12-21 14:43:33 +0100

    Added d3d11 to the projekt (main-file updated)

--- Project Jackalope/main.cpp
+++ Project Jackalope/main.cpp
@@ -1,15 +1,28 @@
-#include <windows.h>
+#include <Windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
 
-LRESULT CALLBACK WindowProc(HWND hWnd,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
 
-int WINAPI WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow)
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+
+// prototypes for now
+void InitD3D(HWND hWnd);  
+void RenderFrame(void);     
+void CleanD3D(void);        
+
+IDXGISwapChain *swapchain;  
+ID3D11Device *dev;                     
+ID3D11DeviceContext *devcon;           
+ID3D11RenderTargetView *backbuffer;
+ID3D11Texture2D *pBackBuffer;
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	HWND hWnd;
 	WNDCLASSEX wc;
@@ -21,14 +34,12 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.lpszClassName = L"WindowClass1";
 
 	RegisterClassEx(&wc);
 
-	RECT wr = { 0, 0, 640, 480 };  
+	RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };  
 	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
-
 														 
 	hWnd = CreateWindowEx(NULL,
 		L"WindowClass1",
@@ -45,6 +56,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	ShowWindow(hWnd, nCmdShow);
 
+	InitD3D(hWnd);
+
 	MSG msg = { 0 };
 
 	while (TRUE)
@@ -57,13 +70,11 @@ int WINAPI WinMain(HINSTANCE hInstance,
 			if (msg.message == WM_QUIT)
 				break;
 		}
-		else
-		{
-			// Run game code here
-			// ...
-			// ...
-		}
+	
+		RenderFrame();
 	}
+
+	CleanD3D();
 
 	return msg.wParam;
 }
@@ -81,4 +92,73 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 	}
 
 	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+void InitD3D(HWND hWnd)
+{
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	scd.BufferCount = 1;                                    
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     
+	scd.BufferDesc.Width = SCREEN_WIDTH;                    
+	scd.BufferDesc.Height = SCREEN_HEIGHT;                  
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;     
+	scd.OutputWindow = hWnd;                              
+	scd.SampleDesc.Count = 4;                               
+	scd.Windowed = TRUE;                                    
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;     
+
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+
+	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+	// Set the viewport
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = SCREEN_WIDTH;
+	viewport.Height = SCREEN_HEIGHT;
+
+	devcon->RSSetViewports(1, &viewport);
+}
+
+// Render single frame
+void RenderFrame(void)
+{
+	float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+	devcon->ClearRenderTargetView(backbuffer, clearColor);
+	swapchain->Present(0, 0);
+}
+
+// Clean up
+void CleanD3D(void)
+{
+	swapchain->SetFullscreenState(FALSE, NULL);		// switch to windowed mode
+
+	swapchain->Release();
+	backbuffer->Release();
+	dev->Release();
+	devcon->Release();
+	pBackBuffer->Release();
 }
