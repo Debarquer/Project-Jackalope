cb46a1c3efb373a955704c1e92357c9f9d6a5f91
Jakobzzz <jakob.ahrneteg@gmail.com>
2015-12-22 13:13:43 +0100

    Render single triangle and added effect file for shaders

--- Project Jackalope/Project Jackalope/Functions.h
+++ Project Jackalope/Project Jackalope/Functions.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <Windows.h>
 
-void InitD3D(HWND hWnd);
-void RenderFrame(void);
-void CleanD3D(void);
+void InitD3D(HWND hWnd);	// set up and init d3d11
+void RenderFrame(void);		// render single frame
+void CleanD3D(void);		// clean up
+void InitGraphics(void);	// creates the shapes to render
+void InitPipeline(void);    // loads and prepares the shaders
--- Project Jackalope/main.cpp
+++ Project Jackalope/main.cpp
@@ -14,6 +14,12 @@ ID3D11Device *dev;
 ID3D11DeviceContext *devcon;           
 ID3D11RenderTargetView *backbuffer;
 ID3D11Texture2D *pBackBuffer;
+ID3D11InputLayout *pLayout;            
+ID3D11VertexShader *pVS;              
+ID3D11PixelShader *pPS;                
+ID3D11Buffer *pVBuffer;   
+
+struct TRIANGLE { float x, y, z; float r, g, b, w; };
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -136,6 +142,9 @@ void InitD3D(HWND hWnd)
 	viewport.Height = SCREEN_HEIGHT;
 
 	devcon->RSSetViewports(1, &viewport);
+
+	InitPipeline();
+	InitGraphics();
 }
 
 // Render single frame
@@ -143,6 +152,14 @@ void RenderFrame(void)
 {
 	float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
 	devcon->ClearRenderTargetView(backbuffer, clearColor);
+
+	// select which vertex buffer to display
+	UINT stride = sizeof(TRIANGLE);
+	UINT offset = 0;
+	devcon->IASetVertexBuffers(0, 1, &pVBuffer, &stride, &offset);
+	devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	devcon->Draw(3, 0);
 	swapchain->Present(0, 0);
 }
 
@@ -151,9 +168,64 @@ void CleanD3D(void)
 {
 	swapchain->SetFullscreenState(FALSE, NULL);		// switch to windowed mode
 
+	pLayout->Release();
+	pVS->Release();
+	pPS->Release();
+	pVBuffer->Release();
 	swapchain->Release();
 	backbuffer->Release();
 	dev->Release();
 	devcon->Release();
 	pBackBuffer->Release();
+}
+
+// Create shapes to render
+void InitGraphics()
+{
+	TRIANGLE OurVertices[] =
+	{
+		{ 0.0f, 0.5f, 0.0f,		1.0f, 0.0f, 0.0f, 1.0f },
+		{ 0.45f, -0.5, 0.0f,	0.0f, 1.0f, 0.0f, 1.0f },
+		{ -0.45f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 1.0f }
+	};
+
+	// create the vertex buffer
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+
+	bd.Usage = D3D11_USAGE_DYNAMIC;                
+	bd.ByteWidth = sizeof(TRIANGLE) * 3;             
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;       
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;   
+
+	dev->CreateBuffer(&bd, NULL, &pVBuffer); 
+
+	// copy the vertices into the buffer
+	D3D11_MAPPED_SUBRESOURCE ms;
+	devcon->Map(pVBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);    
+	memcpy(ms.pData, OurVertices, sizeof(OurVertices));             
+	devcon->Unmap(pVBuffer, NULL);                                    
+}
+
+// Loads and prepares the shaders
+void InitPipeline()
+{
+	ID3D10Blob *VS, *PS;
+	D3DCompileFromFile(L"Effects.fx", 0, 0, "VShader", "vs_4_0", 0, 0, &VS, 0);
+	D3DCompileFromFile(L"Effects.fx", 0, 0, "PShader", "ps_4_0", 0, 0, &PS, 0);
+
+	dev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &pVS);
+	dev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pPS);
+
+	devcon->VSSetShader(pVS, 0, 0);
+	devcon->PSSetShader(pPS, 0, 0);
+
+	D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+
+	dev->CreateInputLayout(ied, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &pLayout);
+	devcon->IASetInputLayout(pLayout);
 }
